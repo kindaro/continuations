f797cc340e5b395d8db05fb1f948e71ef68ee34b
Ignat Insarov <kindaro@gmail.com>
2018-08-02 20:49:14 +0700

    Improve presentation.

--- RecCont.hs
+++ RecCont.hs
@@ -1,49 +1,67 @@
 {-# LANGUAGE Rank2Types #-}
 
+
 module RecCont where
 
--- | I claim there is a functor T from a to T a.
+
+-- | I claim there is an endofunctor T from Hask to a subcategory thereof that consists of
+--   functions of a certain form:
+
 data T a = T { unT :: forall r. (a -> r) -> r }
 
+
 -- | This is the object operation of T.
+
 t :: a -> T a
 t x = T $ \f -> f x
 
+
 -- | `run id` is the inverse of the object operation of T.
+
 run :: (a -> b) -> T a -> b
 run f (T x) = x f
 
--- | This is the morphism operation of T.
-ft :: (a -> b) -> T a -> T b
-ft f = \(T x) -> T $ \g -> x (g . f)
 
--- ^
+-- | This is the morphism operation of T.
+--
 -- λ run id (ft (+10) (t 10))
 -- 20
 
+ft :: (a -> b) -> T a -> T b
+ft f = \(T x) -> T $ \g -> x (g . f)
+
+
 -- | This is the inverse of the morphism operation of T.
+
 runft :: (T a -> T b) -> a -> b
 runft f x = run id $ f (t x)
 
+
 -- | A Kleisli morphism into T.
+
 rec :: Int -> T Int
 rec x = t (x - 1)
 
--- | This is the binding operation in the monad on T.
-bind :: (a -> T b) -> T a -> T b
-bind f x = run f x
 
--- ^
+-- | This is the binding operation in the monad on T.
+--
 -- λ run id $ bind rec . bind rec $ t 10
 -- 8
 --
 -- λ run id $ foldl (.) id (replicate 10 $ bind rec) $ t 10
 -- 0
 
--- | This is the cartesian product on T.
-mf :: T a -> T b -> T (a, b)
-mf x y = t (run id x, run id y)
+bind :: (a -> T b) -> T a -> T b
+bind f x = run f x
 
--- ^
+
+-- | This is the cartesian product on T.
+--
 -- λ run id $ ft (uncurry (+)) $ mf (t 1) (t 2)
 -- 3
+--
+-- λ run print $ ft (uncurry ($)) $ mf (t (+2)) (t 3)
+-- 5
+
+mf :: T a -> T b -> T (a, b)
+mf x y = t (run id x, run id y)
