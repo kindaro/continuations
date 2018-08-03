62b716eb497005703187a465b31f1fdfe6bc82df
Ignat Insarov <kindaro@gmail.com>
2018-08-03 15:37:59 +0700

    Add abstract constructor for T1.

--- Cont1.hs
+++ Cont1.hs
@@ -21,6 +21,18 @@ t :: a -> T1 m a
 t x = T1 $ \f -> f x
 
 
+-- | This is the abstract constructor of T1.
+--
+-- λ :type t1 (\k -> k 2)
+-- t1 (\k -> k 2) :: Num a => T1 m a
+--
+-- λ run print $ t1 (\k -> k 2)
+-- 2
+
+t1 :: (forall b. (a -> m b) -> m b) -> T1 m a
+t1 = T1
+
+
 -- | The only runner available to us in this module will be the runner to the terminal object.
 
 run :: (a -> m ()) -> T1 m a -> m ()
