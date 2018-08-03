fe178205a3a32583f1d7d19b3fee2967b8559a84
Ignat Insarov <kindaro@gmail.com>
2018-08-03 15:36:46 +0700

    Fix types @ Cont1.

--- Cont1.hs
+++ Cont1.hs
@@ -11,13 +11,13 @@ newtype T1 m a = T1 { unT1 :: forall r. (a -> m r) -> m r }
 
 -- | This is the dot operation of T1.
 
-t :: a -> T1 a
+t :: a -> T1 m a
 t x = T1 $ \f -> f x
 
 
 -- | The only runner available to us in this module will be the runner to the terminal object.
 
-run :: (a -> m ()) -> T1 a -> m ()
+run :: (a -> m ()) -> T1 m a -> m ()
 run f (T1 x) = x f
 
 
@@ -26,5 +26,5 @@ run f (T1 x) = x f
 -- λ run id (ft (+10) (t 10))
 -- 20
 
-ft :: (a -> b) -> T1 a -> T1 b
+ft :: (a -> b) -> T1 m a -> T1 m b
 ft f = \(T1 x) -> T1 $ \g -> x (g . f)
